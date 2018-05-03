62ecc3615b2c72fbbc23fb72c69a83b580c1f84f
cacerescoret <cacerescoret@gmail.com>
2018-05-03 20:57:17 +0200

    buscadores2

--- src/app/clientes/listado-clientes/listado-clientes.component.ts
+++ src/app/clientes/listado-clientes/listado-clientes.component.ts
@@ -11,22 +11,124 @@ import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 export class ListadoClientesComponent implements OnInit {
 
   buscador: FormControl;
+  buscadorLocalidad: FormControl;
+  buscNombreLocalidad: FormGroup;
   clientes:any;
   id:string;
+  mensaje:boolean = false;
+  verBuscadorNombre:boolean = true;
+  verBuscadorLocalidad:boolean = false;
+  verBuscadorNombreLocalidad:boolean = false;
+  consulta:any;
+  buscando:boolean = false;
 
-  constructor(private clientesService: ClientesService) { }
+  constructor(private clientesService: ClientesService,
+              private fb: FormBuilder) { }
 
   ngOnInit() {
     this.buscador = new FormControl();
     this.buscador.valueChanges
               .subscribe(nombre =>{
-                this.clientesService.getClientes(nombre)
-                        .subscribe((resp:any)=>{
-                          this.clientes = resp.clientes;
-                        },(error)=>{
-                          console.log(error);
-                        })
+                this.buscando = true;
+                if(nombre.length !== 0){
+                  this.clientesService.getClientes(nombre)
+                  .subscribe((resp:any)=>{
+                    this.buscando = false;
+                    this.clientes = resp.clientes;
+                    if(this.clientes.length === 0){
+                      this.mensaje = true;
+                    } else {
+                      this.mensaje = false;
+                    }
+                  },(error)=>{
+                    this.buscando = false;
+                    console.log(error);
+                  })
+                } else {
+                  this.buscando = false;
+                  this.clientes = [];
+                  this.mensaje = false;
+                }
               })
+    this.buscadorLocalidad = new FormControl();
+    this.buscadorLocalidad.valueChanges
+              .subscribe(localidad =>{
+                this.buscando = true;
+                if(localidad.length !== 0){
+                  this.clientesService.getClientesLocalidad(localidad)
+                  .subscribe((resp:any)=>{
+                    this.buscando = false;
+                    this.clientes = resp.clientes;
+                    if(this.clientes.length === 0){
+                      this.mensaje = true;
+                    } else {
+                      this.mensaje = false;
+                    }
+                  },(error)=>{
+                    this.buscando = false;
+                    console.log(error);
+                  })
+                } else {
+                  this.buscando = false;
+                  this.clientes = [];
+                  this.mensaje = false;
+                }
+              })
+    this.buscNombreLocalidad = this.fb.group({
+      nombre: null,
+      localidad: null
+    })
+  }
+
+  crearConsulta(){
+    this.buscando = true;
+    this.clientes = [];
+    this.consulta = this.guardarConsulta();
+    this.clientesService.getClientesNombreLocalidad(this.consulta)
+                            .subscribe((resp:any)=>{
+                              this.buscando = false;
+                              this.clientes = resp.clientes;
+                              if(this.clientes.length === 0){
+                                this.mensaje = true;
+                              } else {
+                                this.mensaje = false;
+                              }
+                              this.buscNombreLocalidad.reset();
+                            },(error)=>{
+                              this.buscando = false;
+                              console.log(error);
+                            })
+  }
+
+  guardarConsulta(){
+    const guardarConsulta = {
+      nombre: this.buscNombreLocalidad.get('nombre').value,
+      localidad: this.buscNombreLocalidad.get('localidad').value
+    }
+    return guardarConsulta;
+  }
+
+  mostrarBuscadorNombre(){
+    this.verBuscadorNombre = true;
+    this.verBuscadorLocalidad = false;
+    this.verBuscadorNombreLocalidad = false;
+    this.clientes = [];
+    this.buscador.setValue('');
+  }
+
+  mostrarBuscadorLocalidad(){
+    this.verBuscadorNombre = false;
+    this.verBuscadorLocalidad = true;
+    this.verBuscadorNombreLocalidad = false;
+    this.clientes = [];
+    this.buscadorLocalidad.setValue('');
+  }
+
+  mostrarBuscadorNombreLocalidad(){
+    this.verBuscadorNombre = false;
+    this.verBuscadorLocalidad = false;
+    this.verBuscadorNombreLocalidad = true;
+    this.clientes = [];
   }
 
 }
--- src/app/servicios/clientes.service.ts
+++ src/app/servicios/clientes.service.ts
@@ -7,12 +7,36 @@ export class ClientesService {
 
   constructor(private http: HttpClient) { }
 
+  getTodosClientes(){
+    let url = 'http://localhost:3000/cliente';
+    return this.http.get(url)
+                  .map((res:any)=>{
+                    return res;
+                  });
+  }
+
   getClientes(nombre){
-    let url = 'http://localhost:3000/cliente/';
+    let url = 'http://localhost:3000/cliente/nombre/';
     return this.http.get(url+nombre)
                   .map((res:any)=>{
                     return res;
                   });
+  }
+
+  getClientesLocalidad(localidad){
+    let url = 'http://localhost:3000/cliente/localidad/';
+    return this.http.get(url+localidad)
+                  .map((res:any)=>{
+                    return res;
+                  });
+  }
+
+  getClientesNombreLocalidad(consulta){
+    let url = 'http://localhost:3000/cliente/mixto/'+ consulta.nombre + '/' + consulta.localidad;
+    return this.http.get(url)
+                      .map((res:any)=>{
+                        return res;
+                      });
   }
 
   getCliente(id){
